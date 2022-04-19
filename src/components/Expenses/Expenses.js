18efba8ca51a5906c900b91f328d91dc5d890a4a
Anthony Limani <alimani9193@gmail.com>
2022-04-19 09:33:29 -0400

    Created ExpenseFilter component to filter Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,7 @@
+import React, {useState} from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
@@ -7,12 +9,19 @@ const Expenses = (props) => {
 
     const classes = 'expenses ' + props.className;
 
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (year) => {
+        setFilteredYear(year)
+    };
+
     const expenseItems = props.expenses.map((expenseItem) =>
         <ExpenseItem expense={expenseItem}/>
     );
 
     return (
         <Card className={classes}>
+            <ExpensesFilter selected={filteredYear} onFilterByYear={filterChangeHandler}/>
             {expenseItems}
         </Card>
     );
